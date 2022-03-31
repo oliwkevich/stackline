bc12eea4b8407b6af9e8d20830a5e4750161969f
oliwkevich <olishkevich123@gmail.com>
2022-03-31 19:52:13 +0200

    Добавлена страница 404

--- src/pages/404/Page404.jsx
+++ src/pages/404/Page404.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import BG from "../../assets/video.mp4";
+
 export const Page404 = () => {
-    return (
-        <div>
-            <h1>404 Page, Not Found</h1>
-        </div>
-    )
-}
+  return (
+    <div className="w-full h-[90vh] text-white">
+      <video
+        className="object-cover h-full w-full absolute -z-10"
+        src={BG}
+        autoPlay="autoplay"
+        loop
+        muted
+        preload="auto"
+      />
+      <div className='flex flex-col max-w-[1240px] m-auto items-center py-36'>
+        <h1 className='text-9xl'>404</h1>
+        <p className='text-4xl'>Сторінка не знайдена</p>
+        <button className='m-6 hover:animate-pulse'><Link to='/'>Повернутися назад</Link></button>
+      </div>
+    </div>
+  );
+};
